df102be1222c13e9081cf90726b9237f47db4988
Benjamin Vincent <luxcium@neb401.com>
2019-04-27 03:02:35 -0400

    will publish

--- examples/exampleTSX.tsx
+++ examples/exampleTSX.tsx
@@ -3,7 +3,7 @@ var ReactDOM = require("react-dom");
 
 class MyComponent extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return <div classname="name">Hello World</div>;
   }
 }
 
