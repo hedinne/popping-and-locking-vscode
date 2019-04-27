517440ff13870e2688e2dbf4543bfbd30b9edf0e
Benjamin Vincent <luxcium@neb401.com>
2019-04-27 00:38:07 -0400

    ADDING TSX/JSX support

---
+++ examples/exampleTSX.tsx
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById("root"));
